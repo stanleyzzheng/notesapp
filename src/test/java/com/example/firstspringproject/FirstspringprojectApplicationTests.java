70ad1b7576b9fbcee831ce1f86e400e0196357e9
stanford30 <stanleyzheng84@gmail.com>
2023-12-23 23:41:47 -0600

    test

--- src/test/java/com/example/firstspringproject/FirstspringprojectApplicationTests.java
+++ src/test/java/com/example/firstspringproject/FirstspringprojectApplicationTests.java
@@ -1,11 +1,24 @@
 package com.example.firstspringproject;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@WebMvcTest(FirstspringprojectApplication.class)
 class FirstspringprojectApplicationTests {
 
+  @Autowired
+  private MockMvc mockMvc;
+
   @Test
-  void contextLoads() {}
+  void testHomeEndpoint() throws Exception {
+    // Perform GET request to "/" and expect "Hello World!"
+    mockMvc
+      .perform(MockMvcRequestBuilders.get("/"))
+      .andExpect(MockMvcResultMatchers.status().isOk())
+      .andExpect(MockMvcResultMatchers.content().string("Hello World!"));
+  }
 }
